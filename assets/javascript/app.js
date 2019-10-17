9c5be8b97fb568702e630ec356155971b4e9ec7c
Maddie <54539182+maddieclare@users.noreply.github.com>
2019-10-17 20:08:38 +0800

    Basic pseudocode added

---
+++ assets/javascript/app.js
@@ -0,0 +1,24 @@
+// List of trivia questions.
+
+// List of possible answers for each question.
+// Mark one of these answers as correct.
+
+// Timer counting down 10 seconds per question.
+
+// Function tracking player guess.
+
+// Function comparing player guess to correct answer.
+
+// If player guess is the same as the correct answer, display win page.
+
+// Otherwise, if player guess is incorrect, show lose page and display correct answer.
+
+// Show time out page if player does not guess in time.
+
+// Track correct and incorrect answers (timeouts count as incorrect).
+
+// Wait three seconds, then show a new question.
+
+    // Timer is reset to 10 seconds.
+
+// Function to restar the game once all questions have been displayed.
